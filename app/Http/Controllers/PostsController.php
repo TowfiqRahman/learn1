8389e9900dac0fd2cc9747c8d1463caf0f81b384
TowfiqRahman <towfiq.cse.cuet@gmail.com>
2020-07-27 04:21:19 +0600

    created Post Model, dont know how it connected to posts table??

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Post;
 class PostsController extends Controller
 {
     public function show($slug){
 
-      $post = \DB::table('posts')->where('slug', $slug)->first();
+    //  $post = \DB::table('posts')->where('slug', $slug)->first();
+      $post = Post::where('slug', $slug)->firstorFail();
+
+    /*  if(! $post){
+          abort(404);
+      }
+      */
     //  dd($post);
 
     //  $posts = [
