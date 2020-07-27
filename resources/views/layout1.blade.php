d58cca8f81aebebce4143bc317c4b1d1310f8e5c
TowfiqRahman <towfiq.cse.cuet@gmail.com>
2020-07-27 22:43:04 +0600

    16 updated the active link dynamically to so active

--- resources/views/layout1.blade.php
+++ resources/views/layout1.blade.php
@@ -31,9 +31,9 @@ Released   : 20140225
       </div>
       <div id="menu">
         <ul>
-          <li class="current_page_item"><a href="#" accesskey="1" title="">Homepage</a></li>
-          <li><a href="#" accesskey="2" title="">Our Clients</a></li>
-          <li><a href="#" accesskey="3" title="">About Us</a></li>
+          <li class= " {{ Request::path() === 'welcome1' ? 'current_page_item' : '' }}"><a href="/laracast/learn1/public/welcome1" accesskey="1" title="">Homepage</a></li>
+          <li class= " {{ Request::path() === '' ? 'current_page_item' : '' }}"><a href="#" accesskey="2" title="">Our Clients</a></li>
+          <li class= " {{ Request::path() === 'about1' ? 'current_page_item' : '' }}"><a href="/laracast/learn1/public/about1" accesskey="3" title="">About Us</a></li>
           <li><a href="#" accesskey="4" title="">Careers</a></li>
           <li><a href="#" accesskey="5" title="">Contact Us</a></li>
         </ul>
